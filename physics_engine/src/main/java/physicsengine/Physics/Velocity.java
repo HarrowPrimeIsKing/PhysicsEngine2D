7f61baff7444d6a8c040e551c6c05d0b3cd06a43
SwisCheeseAt3 <168587230+jsnticcdn@users.noreply.github.com>
2026-06-04 00:44:16 -0400

    Removed timer

--- physics_engine/src/main/java/physicsengine/Physics/Velocity.java
+++ physics_engine/src/main/java/physicsengine/Physics/Velocity.java
@@ -1,10 +1,8 @@
 package physicsengine.Physics;
-import javax.swing.Timer;
 public class Velocity {
 
     public static double set_velocity(double velocity){
         
-        Timer timer = new Timer(0, null);
         double deltatime = System.nanoTime();
         velocity = (velocity *deltatime)/2;
         return velocity;
